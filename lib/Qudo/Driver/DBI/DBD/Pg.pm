fd2e647e6ab83e1295acf92ea169347312445d38
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2015-08-04 13:51:36 +0900

    余計なデバッグ用の warn が含まれていたので削除

--- lib/Qudo/Driver/DBI/DBD/Pg.pm
+++ lib/Qudo/Driver/DBI/DBD/Pg.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Data::Dumper;
 
-sub last_insert_id { warn Dumper $_[2]; $_[2]->{mysql_insertid} || $_[2]->{insertid} }
+sub last_insert_id { $_[2]->{mysql_insertid} || $_[2]->{insertid} }
 
 sub sql_for_unixtime {
     return "extract(epoch from now())::int";
